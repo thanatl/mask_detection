ec278577c9d2ecb33228fb22cd95124effd82451
thanatlap <thanat@lab.ai>
2020-07-20 14:46:53 +0700

    add dataloader

--- compose_data.py
+++ compose_data.py
@@ -8,46 +8,144 @@ import json
 from glob import glob
 
 
-def create_data_file(self):
+def detect_face(image):
+        '''
+        Return (is_detect, x, y, width, height)
+        '''
 
-        mask_files = sorted(glob(os.path.join('data', 'with_mask','*.jpg')))
-        unmask_files = sorted(glob(os.path.join('data', 'without_mask', '*.jpg')))
+        def _detect_face_opencv(img):
+            face_cascade = cv2.CascadeClassifier(
+                os.path.join('artifacts', 'opencv_frontal_face.xml'))
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            dets = face_cascade.detectMultiScale(gray, 1.1, 4)
+            return dets
 
-        mask_imgs = [cv2.imread(file) for file in mask_files]
-        unmask_imgs = [cv2.imread(file) for file in unmask_files]
+        def _detect_face_dlib(img):
+            detector = dlib.get_frontal_face_detector()
+            dets = detector(img, 1)
+            return dets
 
-        mask_bbox = [self.detect_face(img) for img in mask_imgs]
-        unmask_bbox = [self.detect_face(img) for img in unmask_imgs]
+        # prioritize dlib detector
+        dets = _detect_face_dlib(image)
+        if list(dets) == []:  # if dlib not found face
+            dets = _detect_face_opencv(image)
+            for d in dets:
+                # (x, y, w, h) = d[0]+ d[2]//2, d[1]+ d[3]//2, d[2], d[3] # center xy 
+                (x, y, w, h) = d[0]+ d[2]//2, d[1]+ d[3]//2, d[2], d[3]
+        else:
+            for d in dets:
+                (x, y, w, h) = (d.left(), 
+                                d.top(),
+                                d.right() - d.left(), 
+                                d.bottom() - d.top())
 
-        mask_label = [1]*len(mask_files)
-        unmask_label = [0]*len(mask_files)
+        try:
+            return (1, x, y, w, h)
 
-        # pack data
-        face_data_mask = [data for data in zip(
-            mask_files, mask_bbox, mask_label) if data[1][0] == 1]
-        face_data_unmask = [data for data in zip(
-            unmask_files, unmask_bbox, unmask_label) if data[1][0] == 1]
+        except:
+            return (0, 0, 0, 0, 0)
 
-        # split train test data
-        def _split_train_test(data, prob=0.8):
-            np.random.shuffle(data)
-            train_index = int(prob * len(data))
-            return data[:train_index], data[train_index:]
 
-        train_mask, test_mask = _split_train_test(face_data_mask)
-        train_unmask, test_unmask = _split_train_test(face_data_unmask)
+def create_data_file_1():
 
-        # export data
-        def _export_to_json(data, filename):
-            face_data = {d[0]: {'x': int(d[1][1]),
-                               'y': int(d[1][2]),
-                               'w': int(d[1][3]),
-                               'h': int(d[1][4]),
-                               'mask': int(d[2])}
-                         for idx, d in enumerate(data) if d[1][0] == 1}
-            
-            with open(os.path.join('data', filename), 'w') as f:
-                json.dump(face_data, f)
+    mask_files = sorted(glob(os.path.join('data', 'with_mask','*.jpg')))
+    unmask_files = sorted(glob(os.path.join('data', 'without_mask', '*.jpg')))
 
-        _export_to_json(train_mask+train_unmask, 'train_dataset.json')
-        _export_to_json(test_mask+test_unmask, 'test_dataset.json')
+    mask_imgs = [cv2.imread(file) for file in mask_files]
+    unmask_imgs = [cv2.imread(file) for file in unmask_files]
+
+    mask_bbox = [detect_face(img) for img in mask_imgs]
+    unmask_bbox = [detect_face(img) for img in unmask_imgs]
+
+    mask_label = [1]*len(mask_files)
+    unmask_label = [0]*len(mask_files)
+
+    # pack data
+    face_data_mask = [data for data in zip(
+        mask_files, mask_bbox, mask_label) if data[1][0] == 1]
+    face_data_unmask = [data for data in zip(
+        unmask_files, unmask_bbox, unmask_label) if data[1][0] == 1]
+
+    # split train test data
+    def _split_train_test(data, prob=0.8):
+        np.random.shuffle(data)
+        train_index = int(prob * len(data))
+        return data[:train_index], data[train_index:]
+
+    train_mask, test_mask = _split_train_test(face_data_mask)
+    train_unmask, test_unmask = _split_train_test(face_data_unmask)
+
+    def _to_dict(data):
+        data = {d[0]: { 'x': int(d[1][1]),
+                        'y': int(d[1][2]),
+                        'w': int(d[1][3]),
+                        'h': int(d[1][4]),
+                        'mask': int(d[2])}
+                    for idx, d in enumerate(data) if d[1][0] == 1}
+        return data
+
+    train_data = _to_dict(train_mask+train_unmask)
+    test_data = _to_dict(test_mask+test_unmask)
+
+    return train_data, test_data
+
+
+def create_data_file_2():
+
+    train_image_path =  os.path.join('data', 'yolo', 'images', 'train')
+    train_label_path =  os.path.join('data', 'yolo', 'labels', 'train')
+
+    val_image_path =  os.path.join('data', 'yolo', 'images', 'valid')
+    val_label_path =  os.path.join('data', 'yolo', 'labels', 'valid')
+
+    def _get_data(image_path, label_path):
+
+        images_filepath = sorted(glob(os.path.join(image_path,'*.jpg')))
+        # labels_filepath = sorted(glob(os.path.join(train_label_path,'*.txt')))
+        imgs = [cv2.imread(file) for file in images_filepath]
+        
+        # loop throght image file for consistancy since filename of image and label are the same
+        ob_data = []
+        for file in images_filepath: 
+            with open(os.path.join(label_path,file[len(image_path)+1:-3]+'txt'), 'r') as f:
+                data = [float(coord) for coord in f.read().replace('\n','').split(' ')] + [file] # concat image filepath
+                ob_data.append(data)
+        
+        return ob_data, imgs
+
+    def _to_dict(data, imgs):
+
+        data = {d[5]: { 'x': int(d[1]*imgs[idx].shape[0]) - int(d[3]*imgs[idx].shape[0])//2,
+                        'y': int(d[2]*imgs[idx].shape[1]) - int(d[4]*imgs[idx].shape[1])//2,
+                        'w': int(d[3]*imgs[idx].shape[0]),
+                        'h': int(d[4]*imgs[idx].shape[1]),
+                        'mask': int(d[0])}
+                    for idx, d in enumerate(data)}
+        return data
+    
+    train_data, train_imgs = _get_data(train_image_path, train_label_path)
+    val_data, val_imgs = _get_data(val_image_path, val_label_path)
+    train_data = _to_dict(train_data, train_imgs)
+    val_data = _to_dict(val_data,val_imgs)
+
+    return train_data, val_data
+
+
+def main():
+    print('[INFO] Prep Data #1')
+    train_1, val_1 = create_data_file_1()
+    print('[INFO] Prep Data #2')
+    train_2, val_2 = create_data_file_2()
+    train_dict = {**train_1, **train_2}
+    val_dict = {**val_1, **val_2}
+
+    print('[INFO] Export Data')
+    with open(os.path.join('data', 'train_data.json'), 'w') as f:
+        json.dump(train_dict, f)
+
+    with open(os.path.join('data', 'val_data.json'), 'w') as f:
+        json.dump(val_dict, f)
+
+
+if __name__ == "__main__":
+    main()
--- data_function.py
+++ data_function.py
@@ -11,23 +11,25 @@ from glob import glob
 # In pytorch, the input should be [xmin, ymin, xmax, ymax]
 
 
-class Generator:
+class MaskDataLoader:
 
-    def __init__(self, is_train):
+    def __init__(self, is_train, resize=416):
 
-        self.json_file = os.path.join('data','train_dataset.json') if is_train else os.path.join('data','test_dataset.json')
+        self.resize = resize
+        self.json_file = os.path.join(
+            'data', 'train_data.json') if is_train else os.path.join('data', 'val_data.json')
         if not os.path.isfile(self.json_file):
-            self.create_data_file()
+            raise ValueError('Json {} Not Found'.format(self.json_file))
 
         with open(self.json_file, 'r') as f:
             self.data = json.load(f)
 
         self.image_file = [file for file in self.data.keys()]
         np.random.shuffle(self.image_file)
-         
+
     @staticmethod
     def read_image(file):
-        image = cv2.imread(file) #(height, width, channel)
+        image = cv2.imread(file)  # (height, width, channel)
         # image = image.reshape((image.shape[2], image.shape[0], image.shape[1])) #(channel, height, width)
         return image
 
@@ -37,6 +39,8 @@ class Generator:
     def get_data(self, data_file):
         label = self.data[data_file]
         image = self.read_image(data_file)
+        image, label = resize_img_bbox_letterbox(image, label, self.resize)
+        image = image_pytorch_format(image)
         return image, np.array([label['x'], label['y'], label['w'], label['h'], label['mask']]).astype('float32')
 
     def __getitem__(self, index):
@@ -44,117 +48,6 @@ class Generator:
 
     def __len__(self):
         return len(self.image_file)
-
-    @staticmethod
-    def detect_face(image):
-        '''
-        Return (is_detect, x (center), y (center), width, height)
-        '''
-
-        def _detect_face_opencv(img):
-            face_cascade = cv2.CascadeClassifier(
-                os.path.join('artifacts', 'opencv_frontal_face.xml'))
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            dets = face_cascade.detectMultiScale(gray, 1.1, 4)
-            return dets
-
-        def _detect_face_dlib(img):
-            detector = dlib.get_frontal_face_detector()
-            dets = detector(img, 1)
-            return dets
-
-        # prioritize dlib detector
-        dets = _detect_face_dlib(image)
-        if list(dets) == []:  # if dlib not found face
-            dets = _detect_face_opencv(image)
-            for d in dets:
-                (x, y, w, h) = d[0]+ d[2]//2, d[1]+ d[3]//2, d[2], d[3]
-        else:
-            for d in dets:
-                (x, y, w, h) = (d.left() + (d.right() - d.left())//2, 
-                                d.top() + (d.bottom() - d.top())//2,
-                                d.right() - d.left(), 
-                                d.bottom() - d.top())
-
-        try:
-            return (1, x, y, w, h)
-
-        except:
-            return (0, 0, 0, 0, 0)
-
-    def create_data_file(self):
-
-        mask_files = sorted(glob(os.path.join('data', 'with_mask','*.jpg')))
-        unmask_files = sorted(glob(os.path.join('data', 'without_mask', '*.jpg')))
-
-        mask_imgs = [cv2.imread(file) for file in mask_files]
-        unmask_imgs = [cv2.imread(file) for file in unmask_files]
-
-        mask_bbox = [self.detect_face(img) for img in mask_imgs]
-        unmask_bbox = [self.detect_face(img) for img in unmask_imgs]
-
-        mask_label = [1]*len(mask_files)
-        unmask_label = [0]*len(mask_files)
-
-        # pack data
-        face_data_mask = [data for data in zip(
-            mask_files, mask_bbox, mask_label) if data[1][0] == 1]
-        face_data_unmask = [data for data in zip(
-            unmask_files, unmask_bbox, unmask_label) if data[1][0] == 1]
-
-        # split train test data
-        def _split_train_test(data, prob=0.8):
-            np.random.shuffle(data)
-            train_index = int(prob * len(data))
-            return data[:train_index], data[train_index:]
-
-        train_mask, test_mask = _split_train_test(face_data_mask)
-        train_unmask, test_unmask = _split_train_test(face_data_unmask)
-
-        # export data
-        def _export_to_json(data, filename):
-            face_data = {d[0]: {'x': int(d[1][1]),
-                               'y': int(d[1][2]),
-                               'w': int(d[1][3]),
-                               'h': int(d[1][4]),
-                               'mask': int(d[2])}
-                         for idx, d in enumerate(data) if d[1][0] == 1}
-            
-            with open(os.path.join('data', filename), 'w') as f:
-                json.dump(face_data, f)
-
-        _export_to_json(train_mask+train_unmask, 'train_dataset.json')
-        _export_to_json(test_mask+test_unmask, 'test_dataset.json')
-
-
-def collate_fn(batch):
-    '''
-    Padding and resize image by maximum image size in each batch
-    return tensor of image and bounding box
-    '''
-
-    max_w = 0
-    max_h = 0
-    for img, bbox in batch:
-        max_w = img.shape[2] if img.shape[2] > max_w else max_w
-        max_h = img.shape[1] if img.shape[1] > max_h else max_h
-    size = max_w if max_w > max_h else max_h
-
-    img_tensor = torch.FloatTensor(len(batch), 3, size, size)
-    img_tensor.zero_()
-    bbox_tensor = torch.FloatTensor(len(batch), 5)
-    bbox_tensor.zero_()
-    for idx, data in enumerate(batch): 
-        img, bbox = data
-        img, bbox = resize_img_bbox_letterbox(img, bbox, size)
-        bbox = normalize_bbox(img, bbox)
-        img = image_pytorch_format(img)
-        img = torch.from_numpy(img)
-        bbox = torch.from_numpy(bbox)
-        img_tensor[idx, :, :,:] = img
-        bbox_tensor[idx,:] = bbox
-
-    return img_tensor, bbox_tensor
 
 
 def normalize_bbox(img, bbox):
@@ -167,7 +60,7 @@ def normalize_bbox(img, bbox):
     Return:
     bbox - normalize bounding box according to image size
     '''
-    w,h = img.shape[1], img.shape[0]
+    w, h = img.shape[1], img.shape[0]
 
     bbox[0] = bbox[0]/w
     bbox[2] = bbox[2]/w
@@ -187,36 +80,60 @@ def resize_img_bbox_letterbox(img, bbox, size):
     Return:
     img - resize image as a letter box padding, keep the original aspect ratio of the image and padding the smaller aspect of the image
     bbox - resize bounding box according to the image 
-    
+
     '''
-    
-    w,h = img.shape[1], img.shape[0]
+
+    w, h = img.shape[1], img.shape[0]
     scale = min(size/w, size/h)
     new_w = int(w * scale)
     new_h = int(h * scale)
-    
+
     resized_image = cv2.resize(img, (new_w, new_h))
     print(resized_image.shape)
     canvas = np.full((size, size, 3), 0)
-    canvas[(size-new_h)//2:(size-new_h)//2 + new_h,(size-new_w)//2:(size-new_w)//2 + new_w, :] = resized_image
+    canvas[(size-new_h)//2:(size-new_h)//2 + new_h, (size-new_w) //
+           2:(size-new_w)//2 + new_w, :] = resized_image
     canvas = canvas.astype(np.uint8)
-   
+
     bbox[:4] = bbox[:4] * scale
-    
+
     # add padding h w
-    bbox[:4] += np.array([(size - new_w)/2, (size - new_h)/2, 0, 0]).astype(int)
-    
+    bbox[:4] += np.array([(size - new_w)/2,
+                          (size - new_h)/2, 0, 0]).astype(int)
+
     return canvas, bbox
+
 
 def image_pytorch_format(img):
     return img.reshape((img.shape[2], img.shape[0], img.shape[1]))
+
+
+def to_gpu(x):
+
+    x = x.contiguous()
+    if torch.cuda.is_available():
+        x = x.cuda(non_blocking=True)
+    return torch.autograd.Variable(x)
+
+
+def batch_to_gpu(data):
+    img, target = data
+    x,y,w,h,mask = target
+    img = to_gpu(img).float()
+    x = to_gpu(x).float()
+    y = to_gpu(y).float()
+    w = to_gpu(w).float()
+    h = to_gpu(h).float()
+    mask = to_gpu(mask).float()
+    return img, (x, y, w, h, mask)
+
 
 if __name__ == "__main__":
 
     gen = Generator(is_train=True)
 
     train_dataset = Generator(is_train=True)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=True, 
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=True,
                                                num_workers=0, collate_fn=collate_fn)
 
     flag = True
